1ce78153a79f46dc44597fd4716770190f46d7c5
CHOI <frank.choi@bycn.bouygues-construction.com>
2021-08-05 17:32:53 +0800

    First Commit

---
+++ src/components/AddBook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BookForm from './BookForm';
+
+const AddBook = () => {
+    const handleOnSubmit = (book) => {
+        console.log(book);
+    };
+
+    return (
+        <>
+            <BookForm handleOnSubmit={handleOnSubmit} />
+        </>
+    );
+};
+
+export default AddBook;
