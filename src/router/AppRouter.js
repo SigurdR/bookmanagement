f9d3d9993f66642ae87a6a45ae21e65fc864b3b6
CHOI <frank.choi@bycn.bouygues-construction.com>
2021-08-09 15:31:01 +0800

    Finish Using Context

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,37 +4,24 @@ import Header from '../components/Header';
 import AddBook from '../components/AddBook';
 import BooksList from '../components/BooksList';
 import EditBook from '../components/EditBook';
+import BooksContext from '../context/BooksContext';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 const AppRouter = () => {
-    const [books, setBooks] = useLocalStorage('books', []);
+    const [books, setBooks] = useLocalStorage('books', []); 
     return (
         <BrowserRouter>
             <div>
                 <Header />
                 <div className="main-content">
-                    <Switch>
-                        <Route 
-                            render={(props) => (
-                                <BooksList {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/"
-                            exact={true}
-                        />
-                        <Route 
-                            render={(props) => (
-                                <AddBook {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/add"
-                        />
-                        <Route
-                            render={(props) => (
-                                <EditBook {...props} books={books} setBooks={setBooks} />
-                            )}
-                            path="/edit/:id"
-                        />
-                        <Route component={() => <Redirect to="/"/>} />
-                    </Switch>
+                    <BooksContext.Provider value={{ books, setBooks }}>
+                        <Switch>
+                            <Route component={BooksList} path="/" exact={true} />
+                            <Route component={AddBook} path="/Add" />
+                            <Route component={EditBook} path="/edit/:id"/>
+                            <Route component={() => <Redirect to="/"/>} />
+                        </Switch>
+                    </BooksContext.Provider>
                 </div>
             </div>
         </BrowserRouter>
